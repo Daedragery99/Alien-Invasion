c1f6947be588fd71e8ffe98dff8ead7a355fce7e
Daedragery99 <gergogara022@gmail.com>
2025-12-22 22:27:49 +0100

    ship

---
+++ settings.py
@@ -0,0 +1,6 @@
+class Settings:
+    """Define and store all the game settings"""
+    def __init__(self):
+        self.screen_widht = 1200
+        self.screen_height = 800
+        self.bg_colort = (230, 230, 230)
