070a294c87d7fda26f47a9ffd67ac1c8645768a9
Daedragery99 <gergogara022@gmail.com>
2025-12-28 21:59:35 +0100

    modified:   alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,7 +14,10 @@ class AlienInvasion:
         self.clock = pygame.time.Clock()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((self.settings.screen_widht, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        
+        self.settings.screen_height = self.screen.get_rect().height
+        self.settings.screen_widht = self.screen.get_rect().width
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
@@ -33,25 +36,32 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
                 elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RIGHT:
-                        self.ship.moving_right = True
-                    elif event.key == pygame.K_LEFT:
-                        self.ship.moving_left = True
-                    elif event.key == pygame.K_KP_PLUS:
-                        self.settings.ship_speed += 1.0
+                    self._key_down_events(event)
                 elif event.type == pygame.KEYUP:
-                    if event.key == pygame.K_RIGHT:
-                        self.ship.moving_right = False
-                    if event.key == pygame.K_LEFT:
-                        self.ship.moving_left = False
+                    self._key_up_events(event)
+                    
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_colort)
         self.ship.blitme()
-        
 
         pygame.display.flip() 
+    
+    def _key_down_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_KP_PLUS:
+            self.settings.ship_speed += 1.5
+        elif event.key == pygame.K_q:
+            sys.exit()
 
+    def _key_up_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
 
 if __name__ == '__main__':
