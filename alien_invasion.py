16be34801639582b4fadf784f914b858e0247262
Daedragery99 <gergogara022@gmail.com>
2026-01-03 07:57:38 +0100

    new file:   __pycache__/bullet.cpython-312.pyc modified:   __pycache__/settings.cpython-312.pyc modified:   __pycache__/ship.cpython-312.pyc modified:   alien_invasion.py new file:   bullet.py modified:   images/ship.bmp modified:   settings.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,9 +4,10 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
-    """The class that manges game assests and logic"""
+    """The class that manages game assests and logic"""
 
     def __init__(self):
         
@@ -21,12 +22,14 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
 
     def run_game(self):
         while True:
             self._check_events()
-            self.ship.update()            
+            self.ship.update()
+            self._update_bullets()          
             self._update_screen()
             self.clock.tick(60)
 
@@ -43,6 +46,8 @@ class AlienInvasion:
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_colort)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         self.ship.blitme()
 
         pygame.display.flip() 
@@ -56,12 +61,29 @@ class AlienInvasion:
             self.settings.ship_speed += 1.5
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _key_up_events(self, event):
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
+
+    def _fire_bullet(self):
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        self.bullets.update()
+
+        #get rid of bullets that escape the frame
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+            #print(len(self.bullets))
+
 
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -3,7 +3,14 @@ class Settings:
     def __init__(self):
         self.screen_widht = 1200
         self.screen_height = 800
-        self.bg_colort = (255, 255, 255)
+        self.bg_colort = (127, 127, 127)
 
         #Ship settings
-        self.ship_speed = 1.5
+        self.ship_speed = 6.5
+
+        #bullet settings
+        self.bullet_speed = 12.0
+        self.bullet_widht = 3
+        self.bullet_height = 15
+        self.bullet_color = (0, 172, 245)
+        self.bullets_allowed = 3
